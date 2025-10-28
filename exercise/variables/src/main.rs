5369d966ca6eeba5bf8b93d9424cd05ae8467a9e
dhany <dhany@citra-digital.org>
2025-10-28 11:35:16 +0700

    variables

--- exercise/variables/src/main.rs
+++ exercise/variables/src/main.rs
@@ -1,6 +1,12 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    missiles = 8
-    ready = 2
+    let (mut missiles, ready):(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+    
+    println!("{} missiles left", missiles);
 }
